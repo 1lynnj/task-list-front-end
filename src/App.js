9d9d60c3bd91b291ffb389734d03a0b71aae9bf8
Lynn <lynn@jansheski.com>
2022-12-16 09:40:03 -0800

    added update iscomplete and delete buttons, moved state to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +17,57 @@ const TASKS = [
 ];
 
 const App = () => {
+  const tasksCopy = TASKS.map((task) => {
+    return { ...task };
+  });
+
+  const [tasksList, setTasksList] = useState(tasksCopy);
+
+  const updateComplete = (taskId) => {
+    console.log('update iscomplete called');
+    const newTasksList = [];
+    for (const task of tasksList) {
+      if (task.id !== taskId) {
+        newTasksList.push(task);
+      } else {
+        const newTask = {
+          ...task,
+          isComplete: !task.isComplete,
+        };
+        newTasksList.push(newTask);
+      }
+    }
+    setTasksList(newTasksList);
+  };
+
+  const deleteTask = (taskId) => {
+    console.log(`🌼 ${tasksList}`);
+    console.log('delete task called');
+    const newTasksList = [];
+    for (const task of tasksList) {
+      if (task.id !== taskId) {
+        newTasksList.push(task);
+      }
+    }
+    setTasksList(newTasksList);
+    console.log(newTasksList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {
+            <TaskList
+              tasks={tasksList}
+              updateComplete={updateComplete}
+              deleteTask={deleteTask}
+            />
+          }
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, updateComplete, deleteTask }) => {
+  console.log(updateComplete);
+  const updateCompleteButtonClick = () => {
+    const updatedTask = {
+      id: id,
+      title: title,
+      isComplete: updateComplete,
+      deleteTask: deleteTask,
+    };
+    updateComplete(id, updatedTask);
+  };
+
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => updateCompleteButtonClick()}
       >
         {title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button
+        className="tasks__item__remove button"
+        onClick={() => deleteTask(id)}
+      >
+        x
+      </button>
     </li>
   );
 };
@@ -24,6 +39,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateComplete: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
