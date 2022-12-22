5ebf280730e512c29a3be74634adf03298b50534
Lynn <lynn@jansheski.com>
2022-12-22 07:17:17 -0800

    Moved notes to end of component pages. Cleaned up working code.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,11 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import TaskList from './components/TaskList.js';
 import NewTaskForm from './components/NewTaskForm.js';
-
 import './App.css';
 
-// const TASKS = [
-//   {
-//     id: 1,
-//     title: 'Mow the lawn',
-//     isComplete: false,
-//   },
-//   {
-//     id: 2,
-//     title: 'Cook Pasta',
-//     isComplete: true,
-//   },
-// ];
-
 const App = () => {
-  // const tasksCopy = TASKS.map((task) => {
-  //   return { ...task };
-  // });
   const [tasksList, setTasksList] = useState([]);
-  // Back-end URL
+
   const URL = 'http://127.0.0.1:5000/tasks';
 
   const fetchAllTasks = () => {
@@ -49,21 +32,13 @@ const App = () => {
   useEffect(fetchAllTasks, []);
 
   const updateComplete = (taskId, updatedTask) => {
-    console.log(`3. updateComplete called with ${JSON.stringify(updatedTask)}`);
-
     let status = '';
     if (updatedTask.isComplete) {
-      console.log(
-        `4a. updatedTask.isComplete is ${updatedTask.isComplete}, so mark_complete`
-      );
       status = 'mark_complete';
     } else {
-      console.log(
-        `4bs. updatedTask.isComplete is ${updatedTask.isComplete}, so mark_incomplete`
-      );
       status = 'mark_incomplete';
     }
-    console.log('5. Update the database');
+
     const newTasksList = [];
     axios
       .patch(`${URL}/${taskId}/${status}`)
@@ -72,16 +47,13 @@ const App = () => {
           if (task.id !== taskId) {
             newTasksList.push(task);
           } else {
-            console.log(`6. Updated task in list is ${updatedTask.isComplete}`);
             const newTask = {
               ...task,
               isComplete: updatedTask.isComplete,
             };
-            console.log(`7. newTask is ${JSON.stringify(newTask)}`);
             newTasksList.push(newTask);
           }
         }
-        console.log('8. Update the task list.');
         setTasksList(newTasksList);
       })
       .catch((error) => {
@@ -90,7 +62,6 @@ const App = () => {
   };
 
   const deleteTask = (taskId) => {
-    console.log('deleteTask called');
     axios
       .delete(`${URL}/${taskId}`)
       .then(() => {
@@ -108,10 +79,6 @@ const App = () => {
   };
 
   const addNewTask = (newTask) => {
-    console.log('add new task is called');
-    // newTask = {
-    //   title: '',
-    // };
     axios
       .post(URL, newTask)
       .then((response) => {
@@ -148,3 +115,155 @@ const App = () => {
 };
 
 export default App;
+
+//***************************Code with notes************************************************ */
+// import React from 'react';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import TaskList from './components/TaskList.js';
+// import NewTaskForm from './components/NewTaskForm.js';
+
+// import './App.css';
+
+// // const TASKS = [
+// //   {
+// //     id: 1,
+// //     title: 'Mow the lawn',
+// //     isComplete: false,
+// //   },
+// //   {
+// //     id: 2,
+// //     title: 'Cook Pasta',
+// //     isComplete: true,
+// //   },
+// // ];
+
+// const App = () => {
+//   // const tasksCopy = TASKS.map((task) => {
+//   //   return { ...task };
+//   // });
+//   const [tasksList, setTasksList] = useState([]);
+//   // Back-end URL
+//   const URL = 'http://127.0.0.1:5000/tasks';
+
+//   const fetchAllTasks = () => {
+//     axios
+//       .get(URL)
+//       .then((response) => {
+//         console.log(response);
+//         const tasksAPIResponseCopy = response.data.map((task) => {
+//           task.isComplete = task.is_complete;
+//           delete task['is_complete'];
+//           return {
+//             ...task,
+//           };
+//         });
+//         setTasksList(tasksAPIResponseCopy);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   useEffect(fetchAllTasks, []);
+
+//   const updateComplete = (taskId, updatedTask) => {
+//     // console.log(`3. updateComplete called with ${JSON.stringify(updatedTask)}`);
+
+//     let status = '';
+//     if (updatedTask.isComplete) {
+//       // console.log(
+//       //   `4a. updatedTask.isComplete is ${updatedTask.isComplete}, so mark_complete`
+//       // );
+//       status = 'mark_complete';
+//     } else {
+//       console.log(
+//         `4bs. updatedTask.isComplete is ${updatedTask.isComplete}, so mark_incomplete`
+//       );
+//       status = 'mark_incomplete';
+//     }
+//     // console.log('5. Update the database');
+//     const newTasksList = [];
+//     axios
+//       .patch(`${URL}/${taskId}/${status}`)
+//       .then(() => {
+//         for (const task of tasksList) {
+//           if (task.id !== taskId) {
+//             newTasksList.push(task);
+//           } else {
+//             // console.log(`6. Updated task in list is ${updatedTask.isComplete}`);
+//             const newTask = {
+//               ...task,
+//               isComplete: updatedTask.isComplete,
+//             };
+//             // console.log(`7. newTask is ${JSON.stringify(newTask)}`);
+//             newTasksList.push(newTask);
+//           }
+//         }
+//         // console.log('8. Update the task list.');
+//         setTasksList(newTasksList);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   const deleteTask = (taskId) => {
+//     // console.log('deleteTask called');
+//     axios
+//       .delete(`${URL}/${taskId}`)
+//       .then(() => {
+//         const newTasksList = [];
+//         for (const task of tasksList) {
+//           if (task.id !== taskId) {
+//             newTasksList.push(task);
+//           }
+//         }
+//         setTasksList(newTasksList);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   const addNewTask = (newTask) => {
+//     // console.log('add new task is called');
+//     // newTask = {
+//     //   title: '',
+//     // };
+//     axios
+//       .post(URL, newTask)
+//       .then((response) => {
+//         let task = response.data.task;
+//         task.isComplete = task.is_complete;
+//         const newTasksList = [...tasksList];
+//         newTasksList.push(response.data.task);
+//         setTasksList(newTasksList);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Ada&apos;s Task List</h1>
+//       </header>
+//       <main>
+//         <div>
+//           {
+//             <TaskList
+//               tasks={tasksList}
+//               updateComplete={updateComplete}
+//               deleteTask={deleteTask}
+//             />
+//           }
+//         </div>
+//         <NewTaskForm addTaskCallback={addNewTask}></NewTaskForm>
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/components/Task.js
+++ src/components/Task.js
@@ -5,22 +5,13 @@ import './Task.css';
 
 const Task = ({ id, title, isComplete, updateComplete, deleteTask }) => {
   const updateCompleteButtonClick = () => {
-    console.log(
-      `1. Button was clicked on a task that is ${
-        isComplete ? 'complete' : 'incomplete'
-      }`
-    );
     const updatedTask = {
       id: id,
       title: title,
       isComplete: !isComplete,
       deleteTask: deleteTask,
     };
-    console.log(
-      `2. New task is ${JSON.stringify(
-        updatedTask
-      )}. Next, call updateComplete.`
-    );
+
     updateComplete(id, updatedTask);
   };
 
@@ -53,3 +44,60 @@ Task.propTypes = {
 };
 
 export default Task;
+
+//****************************Code with notes******************************** */
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// import './Task.css';
+
+// const Task = ({ id, title, isComplete, updateComplete, deleteTask }) => {
+//   const updateCompleteButtonClick = () => {
+//     // console.log(
+//     //   `1. Button was clicked on a task that is ${
+//     //     isComplete ? 'complete' : 'incomplete'
+//     //   }`
+//     // );
+//     const updatedTask = {
+//       id: id,
+//       title: title,
+//       isComplete: !isComplete,
+//       deleteTask: deleteTask,
+//     };
+//     // console.log(
+//     //   `2. New task is ${JSON.stringify(
+//     //     updatedTask
+//     //   )}. Next, call updateComplete.`
+//     // );
+//     updateComplete(id, updatedTask);
+//   };
+
+//   const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+
+//   return (
+//     <li key={id} className="tasks__item">
+//       <button
+//         className={`tasks__item__toggle ${buttonClass}`}
+//         onClick={() => updateCompleteButtonClick()}
+//       >
+//         {id} {title}
+//       </button>
+//       <button
+//         className="tasks__item__remove button"
+//         onClick={() => deleteTask(id)}
+//       >
+//         Delete
+//       </button>
+//     </li>
+//   );
+// };
+
+// Task.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   title: PropTypes.string.isRequired,
+//   isComplete: PropTypes.bool.isRequired,
+//   updateComplete: PropTypes.func.isRequired,
+//   deleteTask: PropTypes.func.isRequired,
+// };
+
+// export default Task;
